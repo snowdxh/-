cf4437a0cddc6a5c3fe755ad907c4e87b57c46a0
snowdxh <1138865665@qq.com>
2021-10-12 12:49:05 +0800

    笔记

--- tedu_note/settings.py
+++ tedu_note/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'tedu_note',
         'USER': 'root',
-        'PASSWORD': 'dxh1999815..',
+        'PASSWORD': 'password',
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
